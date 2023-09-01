84b2e6b246c8cdeba99b999ce3f2e6a478754404
fozza <fozza0611@outlook.com>
2023-09-01 23:48:53 +0800

    fix: init project

---
+++ MyPressure/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyPressure
+//
+//  Created by fozza on 2023/9/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyPressure/MyPressureApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPressureApp.swift
+//  MyPressure
+//
+//  Created by fozza on 2023/9/1.
+//
+
+import SwiftUI
+
+@main
+struct MyPressureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
